8df207371b9e0b8b5acdc0c6058b963c66697cdb
joao <joaoantoniosantosalves1@gmail.com>
2025-08-29 13:03:21 -0300

    📝feat: getting out  comments

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::get('relatorio/movimentacoes/geral', [MovimentacaoReportController::class, 'exportPdf']);
 
     Route::get('/', function () {
-        return view('home.home'); // ou outra view que você queira
+        return view('home.home'); 
     });
 
     Route::resource('produtos', ProdutoController::class);
